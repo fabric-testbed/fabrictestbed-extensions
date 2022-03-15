4606ed9d6a45f3a66b51b03b0f289d6d3e4cb7eb
Paul Ruth <paul.ruth@gmail.com>
2022-03-14 20:13:10 -0400

    increased default sleep

--- fabrictestbed_extensions/fablib/slice.py
+++ fabrictestbed_extensions/fablib/slice.py
@@ -821,7 +821,7 @@ class Slice():
 
         logging.debug(f"network_iface_map: {self.network_iface_map}")
 
-    def submit(self, wait=True, wait_timeout=360, wait_interval=10, progress=True, delay_post_boot_config=30):
+    def submit(self, wait=True, wait_timeout=360, wait_interval=10, progress=True, delay_post_boot_config=60):
         from fabrictestbed_extensions.fablib.fablib import fablib
         fabric = fablib()
 
