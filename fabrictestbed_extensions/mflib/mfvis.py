ec71dc9460906dc966e53e9f597a4a99c595144e
Charles Carpenter <cscarp0@g.uky.edu>
2022-09-26 14:59:54 -0400

    Removed depricated version of mfvis. Added licsense header to mfvis.

--- fabrictestbed_extensions/mflib/mfvis.py
+++ fabrictestbed_extensions/mflib/mfvis.py
@@ -1,3 +1,26 @@
+# MIT License
+#
+# Copyright (c) 2022 FABRIC Testbed
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 import json
 import traceback
 import os
@@ -14,9 +37,6 @@ from fabrictestbed_extensions.fablib.fablib import FablibManager as fablib_manag
 from fabrictestbed_extensions.mflib.mflib import mflib
 # For testing
 #from mflib import mflib
-
-# import urllib3
-# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class mfvis(mflib):
     mfvis_sanity_version = "2.02"
