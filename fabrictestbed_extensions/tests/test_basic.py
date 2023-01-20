5b38cce9111e596563c460ae3ce9fce729d53579
Sajith Sasidharan <sajith@hcoop.net>
2023-01-20 17:17:05 -0600

    Run black and isort

--- fabrictestbed_extensions/tests/test_basic.py
+++ fabrictestbed_extensions/tests/test_basic.py
@@ -3,7 +3,9 @@ import pathlib
 import unittest
 
 from fabrictestbed.util.constants import Constants
+
 from fabrictestbed_extensions.fablib.fablib import FablibManager
+
 
 class FablibManagerTests(unittest.TestCase):
     """
@@ -20,7 +22,7 @@ class FablibManagerTests(unittest.TestCase):
         # Constants.FABRIC_TOKEN_LOCATION,
         FablibManager.FABRIC_BASTION_HOST,
         FablibManager.FABRIC_BASTION_USERNAME,
-        FablibManager.FABRIC_BASTION_KEY_LOCATION
+        FablibManager.FABRIC_BASTION_KEY_LOCATION,
     ]
 
     def setUp(self):
@@ -33,27 +35,27 @@ class FablibManagerTests(unittest.TestCase):
     def test_fablib_manager_one_env_var(self):
         for var in self.required_env_vars:
             with self.assertRaises(AttributeError):
-                os.environ[var] = 'dummy'
+                os.environ[var] = "dummy"
                 FablibManager()
 
     def test_fablib_manager_test_only_cm_host(self):
         with self.assertRaises(AttributeError):
-            os.environ[Constants.FABRIC_CREDMGR_HOST] = 'dummy'
+            os.environ[Constants.FABRIC_CREDMGR_HOST] = "dummy"
             FablibManager()
 
     def test_fablib_manager_test_only_orchestrator_host(self):
         with self.assertRaises(AttributeError):
-            os.environ[Constants.FABRIC_ORCHESTRATOR_HOST] = 'dummy'
+            os.environ[Constants.FABRIC_ORCHESTRATOR_HOST] = "dummy"
             FablibManager()
 
     def test_fablib_manager_test_only_project_id(self):
         with self.assertRaises(AttributeError):
-            os.environ[Constants.FABRIC_PROJECT_ID] = 'dummy'
+            os.environ[Constants.FABRIC_PROJECT_ID] = "dummy"
             FablibManager()
 
     def test_fablib_manager_test_only_token_location(self):
         with self.assertRaises(AttributeError):
-            os.environ[Constants.FABRIC_TOKEN_LOCATION] = 'dummy'
+            os.environ[Constants.FABRIC_TOKEN_LOCATION] = "dummy"
             FablibManager()
 
     def test_fablib_manager_test_with_bad_token(self):
@@ -61,10 +63,10 @@ class FablibManagerTests(unittest.TestCase):
             # FablibManager() without a valid token or token location
             # should raise a "ValueError: Invalid value for
             # `refresh_token`, must not be `None`"
-            os.environ[Constants.FABRIC_CREDMGR_HOST] = 'dummy'
-            os.environ[Constants.FABRIC_ORCHESTRATOR_HOST] = 'dummy'
-            os.environ[Constants.FABRIC_PROJECT_ID] = 'dummy'
-            os.environ[Constants.FABRIC_TOKEN_LOCATION] = 'dummy'
+            os.environ[Constants.FABRIC_CREDMGR_HOST] = "dummy"
+            os.environ[Constants.FABRIC_ORCHESTRATOR_HOST] = "dummy"
+            os.environ[Constants.FABRIC_PROJECT_ID] = "dummy"
+            os.environ[Constants.FABRIC_TOKEN_LOCATION] = "dummy"
             FablibManager()
 
     def test_fablib_manager_test_with_dummy_token(self):
@@ -76,11 +78,11 @@ class FablibManagerTests(unittest.TestCase):
         with self.assertRaises(Exception):
             # The real exception is urllib3.exceptions.MaxRetryError,
             # since 'dummy' is an invalid host.
-            os.environ[Constants.FABRIC_CREDMGR_HOST] = 'dummy'
-            os.environ[Constants.FABRIC_ORCHESTRATOR_HOST] = 'dummy'
-            os.environ[Constants.FABRIC_PROJECT_ID] = 'dummy'
+            os.environ[Constants.FABRIC_CREDMGR_HOST] = "dummy"
+            os.environ[Constants.FABRIC_ORCHESTRATOR_HOST] = "dummy"
+            os.environ[Constants.FABRIC_PROJECT_ID] = "dummy"
 
-            path = os.path.join(os.path.dirname(__file__), 'dummy-token.json')
+            path = os.path.join(os.path.dirname(__file__), "dummy-token.json")
             os.environ[Constants.FABRIC_TOKEN_LOCATION] = path
 
             FablibManager()
