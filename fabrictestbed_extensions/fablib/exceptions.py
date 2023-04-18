96b9a19539306460ad6c4d690c9e3923f05ee8fd
Sajith Sasidharan <sajith@hcoop.net>
2023-04-18 16:06:48 -0500

    Return string representation of the exception

--- fabrictestbed_extensions/fablib/exceptions.py
+++ fabrictestbed_extensions/fablib/exceptions.py
@@ -34,3 +34,6 @@ class FablibConfigurationError(Exception):
         """
         self.message = message
         self.errors = errors
+
+    def __str__(self):
+        return f"{self.message}: {self.errors}"
