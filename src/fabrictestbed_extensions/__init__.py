239ac06591ec9be15abe0c545002d1db43919e76
Sajith Sasidharan <sajith@hcoop.net>
2023-06-07 08:44:58 -0500

    Reformat

--- src/fabrictestbed_extensions/__init__.py
+++ src/fabrictestbed_extensions/__init__.py
@@ -1,7 +1,5 @@
 import warnings
 
 warnings.warn(
-    "fabrictestbed_extensions module is deprecated",
-    DeprecationWarning,
-    stacklevel=2
+    "fabrictestbed_extensions module is deprecated", DeprecationWarning, stacklevel=2
 )
