539e768ab9893d0d559a4ffb58bbdbaafa69571d
Sajith Sasidharan <sajith@hcoop.net>
2023-06-07 11:35:48 -0500

    Use single source for version
    Do not use setuptools_scm, because it adds a new thing that we will
    need to get used to.  Instead we will just read the version from
    __init__.py as we are used to.
    
    See:
    https://packaging.python.org/en/latest/guides/single-sourcing-package-version/

--- src/fabrictestbed_extensions/__init__.py
+++ src/fabrictestbed_extensions/__init__.py
@@ -3,3 +3,7 @@ import warnings
 warnings.warn(
     "fabrictestbed_extensions module is deprecated", DeprecationWarning, stacklevel=2
 )
+
+import fabrictestbed_fablib
+
+__version__ = fabrictestbed_fablib.__version__
