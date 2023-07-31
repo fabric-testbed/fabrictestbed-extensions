9e8199136ad536593aac993562aeec08c0ce2360
Sajith Sasidharan <sajith@hcoop.net>
2023-07-31 18:18:42 -0500

    Fix Node.ip_addr_del() docstring

--- fabrictestbed_extensions/fablib/node.py
+++ fabrictestbed_extensions/fablib/node.py
@@ -2234,10 +2234,13 @@ class Node:
     ):
         """
         Delete an IP to an interface on the node.
+
         :param addr: IP address
-        :type addr:  IPv4Address or IPv6Address
+        :type addr: IPv4Address or IPv6Address
+
         :param subnet: subnet.
         :type subnet: IPv4Network or IPv6Network
+
         :param interface: the FABlib interface.
         :type interface: Interface
         """
