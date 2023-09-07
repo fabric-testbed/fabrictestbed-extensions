854c3a7aa94689ca9931ac480f7ec3e50f29649a
Sajith Sasidharan <sajith@hcoop.net>
2023-09-07 15:18:31 -0500

    Add a note about when to call Node.config()

--- fabrictestbed_extensions/fablib/node.py
+++ fabrictestbed_extensions/fablib/node.py
@@ -2993,6 +2993,10 @@ class Node:
         """
         Run configuration tasks for this node.
 
+        Use this method in order to re-apply configuration to a
+        rebooted node.  Normally this method is invoked by
+        ``Slice.submit()`` or ``Slice.modify()``.
+
         Configuration tasks include:
 
             - Setting hostname.
