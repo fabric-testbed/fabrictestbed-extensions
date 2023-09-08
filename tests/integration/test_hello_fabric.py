cdf4f5a9d14eecc012e0baec5ffef11914666239
Sajith Sasidharan <sajith@hcoop.net>
2023-09-08 17:39:52 -0500

    Avoid mixing pytest fixtures and unittest

--- tests/integration/test_hello_fabric.py
+++ tests/integration/test_hello_fabric.py
@@ -21,50 +21,40 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import unittest
-
+from fabrictestbed_extensions.fablib.fablib import FablibManager
 from fabrictestbed_extensions.fablib.node import Node
 from fabrictestbed_extensions.fablib.slice import Slice
 
 
-class HelloFabricTests(unittest.TestCase):
+def test_fablib_hello(fablib, fabric_slice):
     """
-    Run some basic tests against the testbed.
+    Create a slice with a single node, and echo a message from the node.
     """
 
-    def test_fablib_hello(self, fablib, fabric_slice):
-        """
-        Create a slice with a single node, and echo a message from the node.
-        """
-        self.assertIsInstance(fabric_slice, Slice)
+    assert isinstance(fablib, FablibManager)
+    assert isinstance(fabric_slice, Slice)
 
-        # Add a node.
-        node_name = "node-1"
-        site_name = fablib.get_random_site()
-        slice_name = fabric_slice.get_name()
+    # Add a node.
+    node_name = "node-1"
+    site_name = fablib.get_random_site()
+    slice_name = fabric_slice.get_name()
 
-        print(
-            f"Adding node '{node_name}' at site '{site_name}' to slice '{slice_name}'.."
-        )
-        node = fabric_slice.add_node(name=node_name, site=site_name)
+    print(f"Adding node '{node_name}' at site '{site_name}' to slice '{slice_name}'..")
 
-        self.assertIsInstance(node, Node)
+    node = fabric_slice.add_node(name=node_name, site=site_name)
+    assert isinstance(node, Node)
 
-        # Submit the slice.
-        print(f"Submitting slice '{slice_name}'..")
-        fabric_slice.submit()
+    # Submit the slice.
+    print(f"Submitting slice '{slice_name}'..")
+    fabric_slice.submit()
 
-        print(f"Slice '{slice_name}' status:")
-        fabric_slice.show()
+    print(f"Slice '{slice_name}' status:")
+    fabric_slice.show()
 
-        print(f"Testing node '{node_name}' on slice '{slice_name}'...")
+    print(f"Testing node '{node_name}' on slice '{slice_name}'...")
 
-        for node in fabric_slice.get_nodes():
-            stdout, stderr = node.execute("echo Hello, FABRIC from node `hostname -s`")
+    for node in fabric_slice.get_nodes():
+        stdout, stderr = node.execute("echo Hello, FABRIC from node `hostname -s`")
 
-            self.assertEqual(stdout, f"Hello, FABRIC from node {node_name}\n")
-            self.assertEqual(stderr, "")
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert stdout == f"Hello, FABRIC from node {node_name}\n"
+        assert stderr == ""
