c9160d1a7cc94a7826178c7f9138575403d26eb7
Sajith Sasidharan <sajith@hcoop.net>
2023-09-11 11:33:34 -0500

    Move version string to pyproject.toml

--- fabrictestbed_extensions/__init__.py
+++ fabrictestbed_extensions/__init__.py
@@ -1,3 +1,5 @@
-# Please update version number here when making a release: flit, the
-# build backend we use, picks up the version from here.
-__version__ = "1.5.5"
+__all__ = ["__version__"]
+
+from importlib.metadata import version
+
+__version__ = version(__name__)
